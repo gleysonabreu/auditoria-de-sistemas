7a5f1fa5646cd2c72c227cd9872767b08c2ede8c
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-08-17 18:51:07 -0300

    Add tsconfig-paths

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import ReadFile from './classes/ReadFile';
+import ReadFile from '@classes/ReadFile';
 
 try {
   const readFile = new ReadFile('text.txt');
-  readFile.readLines(['hey']);
+  readFile.readLines();
 } catch (error) {
   console.error(error.message);
 }
